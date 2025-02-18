437ca14bff7d0b0db5df5dfcee9c06a605cb10b2
IanSkelskey <ianskelskey@gmail.com>
2025-02-18 13:13:14 -0500

    Improve config validation by checking empty string values
    Modified `check_config` in `Utils.pm` to include empty string checks when verifying configuration values. This enhances validation accuracy by ensuring non-empty configuration values are required.
    
    Release-Note: Add empty string check for configuration validation in Utils
    
    Signed-off-by: Ian Skelskey <ianskelskey@gmail.com>

--- lib/Utils.pm
+++ lib/Utils.pm
@@ -56,7 +56,7 @@ sub check_config {
         # print each one:
         print "Required config: $reqs[$i]\n";
         print "Config value: " . (defined $conf->{ $reqs[$i] } ? $conf->{ $reqs[$i] } : 'undefined') . "\n";
-        push( @missing, $reqs[$i] ) if ( !$conf->{ $reqs[$i] } );
+        push( @missing, $reqs[$i] ) if ( !defined $conf->{ $reqs[$i] } || $conf->{ $reqs[$i] } eq '' );
     }
 
     if ( $#missing > -1 ) {
