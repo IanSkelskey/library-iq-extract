caddb148b35eaadb2471a85b3a0e6907a3245b7e
Ian Skelskey <ianskelskey@gmail.com>
2025-02-20 11:46:50 -0500

    Improve extract_libraryiq.pl documentation
    Added additional documentation to extract_libraryiq.pl script for clarity.
    
    - Provides an overview of the script's purpose in extracting data from Evergreen ILS and sending it to LibraryIQ.
    - Clarifies usage scenarios, especially where direct database access is restricted.
    - Includes details on how the script should be run and data transmitted via SFTP for secure transfer.
    
    Release-Note: Enhance extract_libraryiq.pl script documentation for better understanding and use.
    
    Signed-off-by: Ian Skelskey <ianskelskey@gmail.com>

--- extract_libraryiq.pl
+++ extract_libraryiq.pl
@@ -5,6 +5,11 @@
 # Author: Ian Skelskey
 # Copyright (C) 2024 Bibliomation Inc.
 #
+# This script extracts data from Evergreen ILS and sends it to LibraryIQ.
+# For use in cases when your security policy does not allow direct access to
+# the database. The script can be run on a server with access to the database
+# and the extracted data can be sent to LibraryIQ via SFTP.
+#
 # This program is free software; you can redistribute it and/or modify it 
 # under the terms of the GNU General Public License as published by the 
 # Free Software Foundation; either version 2 of the License, or (at your 
