61e850a485872266786851f2d61461338330e38a
IanSkelskey <ianskelskey@gmail.com>
2025-02-28 15:04:57 -0500

    Add File::Basename for path manipulation in SFTP module
    - Imported File::Basename to utilize the basename function for extracting file names
    - Replaced custom _basename function with the built-in basename function for reliable path handling
    - Ensured compatibility with the existing SFTP upload process
    
    Signed-off-by: Ian Skelskey <ianskelskey@gmail.com>

--- lib/SFTP.pm
+++ lib/SFTP.pm
@@ -3,11 +3,11 @@ package SFTP;
 use strict;
 use warnings;
 use Net::SFTP::Foreign;
+use File::Basename;  # Add this line to import the basename function
 use Exporter 'import';
-use Logging qw(logmsg);  # Add this line to import the logger
+use Logging qw(logmsg);
 
 our @EXPORT_OK = qw(do_sftp_upload);
-
 
 # ----------------------------------------------------------
 # do_sftp_upload
@@ -21,7 +21,7 @@ sub do_sftp_upload {
         return "SFTP connection failed: " . $sftp->error;
     }
 
-    my $remote_path = "$remote_dir/" . _basename($local_file);
+    my $remote_path = "$remote_dir/" . basename($local_file);  # Use basename from File::Basename
     $sftp->put($local_file, $remote_path)
         or return "SFTP upload of $local_file failed: " . $sftp->error;
 
